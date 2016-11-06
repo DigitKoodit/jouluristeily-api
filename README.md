bed3a77b0c90c0c8f6bc70e567adbbad6327933e
Julius Rajala <jijraj@utu.fi>
2016-11-06 23:55:58 +0200

    Initial commit for JR-api.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Jouluristeily API
+
+Ridiculously simple rest-api to serve data to Jouluristeily data to apps.
+
+## Dependencies
+Uses hapi to provide a rest-api
---
+++ server.js
@@ -0,0 +1,43 @@
+const Hapi = require('hapi');
+
+const mockData = {
+  timeStamp: "0",
+  events: {
+    alkumalja: {
+      name: 'Alkumalja',
+      time: '1212321',
+      duration: '3600000',
+      description: 'Jotakin maljaa jotakin'
+    },
+    tapahtumaKaksi: {
+      name: 'Toinen tapahtuma',
+      time: '12321321',
+      duration: '10',
+      description: 'Tapahtuma 2 tapahtuu'
+    }
+  }
+}
+
+function runServer(){
+  const server = new Hapi.Server();
+
+  server.connection({
+    host: 'localhost',
+    port: 80
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/events',
+    handler: (request, reply) => {
+      reply(mockData);
+    }
+  })
+
+  server.start((err) => {
+    if(err) throw err;
+    console.log('Server running at:', server.info.uri);
+  });
+}
+
+runServer();
